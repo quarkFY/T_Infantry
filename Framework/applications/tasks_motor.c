03af2e6055f26116cd44d7e6c6e1a02df279cc20
Chen Xiaofeng <515318881@qq.com>
2017-11-22 15:22:21 +0800

    零点调一下

--- Framework/applications/tasks_motor.c
+++ Framework/applications/tasks_motor.c
@@ -60,7 +60,7 @@ fw_PID_Regulator_t pitchPositionPID = fw_PID_INIT(8.0, 0.0, 0.0, 10000.0, 10000.
 fw_PID_Regulator_t yawPositionPID = fw_PID_INIT(5.0, 0.0, 0.5, 10000.0, 10000.0, 10000.0, 10000.0);//等幅振荡P37.3 I11.9 D3.75  原26.1 8.0 1.1
 fw_PID_Regulator_t pitchSpeedPID = fw_PID_INIT(40.0, 0.0, 15.0, 10000.0, 10000.0, 10000.0, 3500.0);
 fw_PID_Regulator_t yawSpeedPID = fw_PID_INIT(30.0, 0.0, 5, 10000.0, 10000.0, 10000.0, 4000.0);
-#define yaw_zero 4708//100
+#define yaw_zero 4617//100
 #define pitch_zero 6400
 #endif
 //底盘速度PID
