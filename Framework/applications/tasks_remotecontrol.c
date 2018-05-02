3f6f731f8def95b7c173ab3711edc0ae8aa1dc2b
746144870@qq.com <746144870@qq.com>
2018-05-02 22:50:13 +0800

    底盘云台联调V1

--- Framework/applications/tasks_remotecontrol.c
+++ Framework/applications/tasks_remotecontrol.c
@@ -219,7 +219,7 @@ void RemoteControlProcess(Remote *rc)
 		ChassisSpeedRef.left_right_ref   = (rc->ch0 - (int16_t)REMOTE_CONTROLLER_STICK_OFFSET) * STICK_TO_CHASSIS_SPEED_REF_FACT; 
 		
  		pitchAngleTarget -= (rc->ch3 - (int16_t)REMOTE_CONTROLLER_STICK_OFFSET) * STICK_TO_PITCH_ANGLE_INC_FACT;
-		yawAngleTarget   -= (rc->ch2 - (int16_t)REMOTE_CONTROLLER_STICK_OFFSET) * STICK_TO_YAW_ANGLE_INC_FACT; 
+		//yawAngleTarget   -= (rc->ch2 - (int16_t)REMOTE_CONTROLLER_STICK_OFFSET) * STICK_TO_YAW_ANGLE_INC_FACT; 
 		
 		ChassisSpeedRef.rotate_ref   = (rc->ch2 - (int16_t)REMOTE_CONTROLLER_STICK_OFFSET) *STICK_TO_CHASSIS_SPEED_REF_FACT ;
   	//yawAngleTarget = -ChassisSpeedRef.rotate_ref * forward_kp / 2000;
