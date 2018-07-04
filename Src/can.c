2b898c29d5547b98be8d3f9e0cc6a410955571d9
quarkFY <1553563326@qq.com>
2018-07-04 15:38:55 +0800

    cube的can配置需要手动修复

--- Src/can.c
+++ Src/can.c
@@ -60,6 +60,49 @@ CAN_HandleTypeDef hcan1;
 CAN_HandleTypeDef hcan2;
 
 /* CAN1 init function */
+//void MX_CAN1_Init(void)
+//{
+
+//  hcan1.Instance = CAN1;
+//  hcan1.Init.Prescaler = 3;
+//  hcan1.Init.Mode = CAN_MODE_NORMAL;
+//  hcan1.Init.SyncJumpWidth = CAN_SJW_1TQ;
+//  hcan1.Init.TimeSeg1 = CAN_BS1_9TQ;
+//  hcan1.Init.TimeSeg2 = CAN_BS2_4TQ;
+//  hcan1.Init.TimeTriggeredMode = DISABLE;
+//  hcan1.Init.AutoBusOff = DISABLE;
+//  hcan1.Init.AutoWakeUp = DISABLE;
+//  hcan1.Init.AutoRetransmission = DISABLE;
+//  hcan1.Init.ReceiveFifoLocked = DISABLE;
+//  hcan1.Init.TransmitFifoPriority = DISABLE;
+//  if (HAL_CAN_Init(&hcan1) != HAL_OK)
+//  {
+//    _Error_Handler(__FILE__, __LINE__);
+//  }
+
+//}
+///* CAN2 init function */
+//void MX_CAN2_Init(void)
+//{
+
+//  hcan2.Instance = CAN2;
+//  hcan2.Init.Prescaler = 3;
+//  hcan2.Init.Mode = CAN_MODE_NORMAL;
+//  hcan2.Init.SyncJumpWidth = CAN_SJW_1TQ;
+//  hcan2.Init.TimeSeg1 = CAN_BS1_9TQ;
+//  hcan2.Init.TimeSeg2 = CAN_BS2_4TQ;
+//  hcan2.Init.TimeTriggeredMode = DISABLE;
+//  hcan2.Init.AutoBusOff = DISABLE;
+//  hcan2.Init.AutoWakeUp = DISABLE;
+//  hcan2.Init.AutoRetransmission = DISABLE;
+//  hcan2.Init.ReceiveFifoLocked = DISABLE;
+//  hcan2.Init.TransmitFifoPriority = DISABLE;
+//  if (HAL_CAN_Init(&hcan2) != HAL_OK)
+//  {
+//    _Error_Handler(__FILE__, __LINE__);
+//  }
+
+//}
 void MX_CAN1_Init(void)
 {
 
@@ -103,7 +146,6 @@ void MX_CAN2_Init(void)
   }
 
 }
-
 static uint32_t HAL_RCC_CAN1_CLK_ENABLED=0;
 
 void HAL_CAN_MspInit(CAN_HandleTypeDef* canHandle)
@@ -184,11 +226,10 @@ void HAL_CAN_MspDeInit(CAN_HandleTypeDef* canHandle)
 
   /* USER CODE END CAN1_MspDeInit 0 */
     /* Peripheral clock disable */
-    /* Be sure that all peripheral instances that share the same clock need to be disabled */
-    /**  HAL_RCC_CAN1_CLK_ENABLED--;
-    *  if(HAL_RCC_CAN1_CLK_ENABLED==0){
-    *    __HAL_RCC_CAN1_CLK_DISABLE();
-    **/
+    HAL_RCC_CAN1_CLK_ENABLED--;
+    if(HAL_RCC_CAN1_CLK_ENABLED==0){
+      __HAL_RCC_CAN1_CLK_DISABLE();
+    }
   
     /**CAN1 GPIO Configuration    
     PD0     ------> CAN1_RX
@@ -210,11 +251,10 @@ void HAL_CAN_MspDeInit(CAN_HandleTypeDef* canHandle)
   /* USER CODE END CAN2_MspDeInit 0 */
     /* Peripheral clock disable */
     __HAL_RCC_CAN2_CLK_DISABLE();
-    /* Be sure that all peripheral instances that share the same clock need to be disabled */
-    /**  HAL_RCC_CAN1_CLK_ENABLED--;
-    *  if(HAL_RCC_CAN1_CLK_ENABLED==0){
-    *    __HAL_RCC_CAN1_CLK_DISABLE();
-    **/
+    HAL_RCC_CAN1_CLK_ENABLED--;
+    if(HAL_RCC_CAN1_CLK_ENABLED==0){
+      __HAL_RCC_CAN1_CLK_DISABLE();
+    }
   
     /**CAN2 GPIO Configuration    
     PB12     ------> CAN2_RX
